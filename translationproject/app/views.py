33eb61ae36f1965d7e7c05ffc4cc33a2bf3b971f
Mantas Skara <mantassk@unity3d.com>
2023-02-10 12:04:43 +0200

    First example

--- translationproject/app/views.py
+++ translationproject/app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.utils.translation import gettext as _
 
 
 def index(request):
+    example_sentence = 'Welcome to my site.'
 
-    return render(request, 'index.html')
+    # example_variable = _(example_sentence)
+
+    context = {
+        'sentence': example_sentence
+    }
+    return render(request, 'index.html', context=context)
