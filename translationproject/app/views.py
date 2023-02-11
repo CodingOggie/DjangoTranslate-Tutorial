9501231399d16c74321d9c4cac1249be154d5333
Mantas Skara <mantassk@unity3d.com>
2023-02-11 16:38:12 +0200

    ready for lesson

--- translationproject/app/views.py
+++ translationproject/app/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
+import random
 
 
 def index(request):
+    # 1. show simple translation
+    # 2. show manage.py (and install gettext)
+    # 3. show compile.py
     example_sentence = _('Welcome to my site.')
+
+    m = 2
+    d = 13
+
+    example_variable_sentence = _('Today is %(month)s %(day)s.') % {
+        'month': m, 'day': d}
 
     # example_variable = _(example_sentence)
 
     context = {
-        'sentence': example_sentence
+        'sentence': example_sentence,
+        'random_int': random.random(),
+        'example_variable_sentence': example_variable_sentence
     }
     return render(request, 'index.html', context=context)
